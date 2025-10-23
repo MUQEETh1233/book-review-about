ad92bd8315723d952acd8d38cc7485db52dad7e9
MUQEETh1233 <muqeeth1805@gmail.com>
2025-10-23 22:58:11 +0530

    Initial commit - Flask app

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+reviews = []
+
+@app.route('/')
+def home():
+    return render_template('home.html', reviews=reviews)
+
+@app.route('/add', methods=['POST'])
+def add():
+    name = request.form['name']
+    book = request.form['book']
+    review = request.form['review']
+    reviews.append({'name': name, 'book': book, 'review': review})
+    return render_template('home.html', reviews=reviews)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
